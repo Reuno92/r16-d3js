7c215eec811de904db45ea1edb813a17b513db99
renauddev <racinet.renaud@orange.Fr>
2021-06-14 16:58:34 +0200

    feat(global): try to build a face

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-
+import * as d3 from "d3";
 function App() {
+
+  const width = "1000"
+  const height = "800"
+  const centerX = +width / 2;
+  const centerY = +height / 2;
+  const strokeWidth = 15;
+
+  const mouthArc = d3.arc()
+      .innerRadius(90)
+      .outerRadius(100)
+      .startAngle(Math.PI / 2)
+      .endAngle(Math.PI * 3 / 2);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg width={width} height={height}>
+        <g transform={`translate(${centerX}, ${centerY})`}>
+            <circle
+                cx={centerX / 50 }
+                cy={centerY / 60 }
+                r="245"
+                fill="yellow"
+                stroke="black"
+                strokeWidth={strokeWidth}
+            />
+            <circle
+                cx={centerX / 50 - 100}
+                cy={centerY / 60 - 100}
+                r="50"
+            />
+            <circle
+                cx={centerX / 50 + 100}
+                cy={centerX / 50 - 100 }
+                r="50"
+            />
+
+          <path d={mouthArc()} />
+        </g>
+      </svg>
     </div>
   );
 }
